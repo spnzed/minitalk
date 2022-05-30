a9b157b34cc78e9f3cc3769005c3bf0734e6e6d1
Aaron Espinosa <95354392+spnzed@users.noreply.github.com>
2022-05-30 18:20:16 +0200

    Add files via upload

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: aaespino <aaespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 17:01:52 by aaespino          #+#    #+#             */
-/*   Updated: 2022/05/19 20:25:55 by aaespino         ###   ########.fr       */
+/*   Updated: 2022/05/30 18:19:52 by aaespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,52 @@
 #include <signal.h>
 #include <stdio.h>
 
+void message_bin(int pid, char c)
+{
+	int i;
+	int bit;
+	i = 7;
+	while(i >= 0)
+	{
+		//(c >> i) nos coloca en la ultima posicion de los binarios
+		// el & 1 traduce a binario el caracter
+		bit = (c >> i) & 1;
+		//dependiendo del numero mandaremos por una señal u otra al servidor
+		if (bit == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		//vamos restando la i para recorrer la string de final a inicio
+		--i;
+		usleep(150);
+	}
+}
+
+//esta funcion recorre toda la string mandada
+//(que es el segundo argumento) y la manda a 
+//la funcion message_bin con un contador basico
+void message_char(int pid, char *str)
+{
+	int i;
+	i = 0;
+	while (str[i])
+	{
+		message_bin(pid, str[i]);
+		i++;
+	}
+}
+
 int	main(int argc, char *argv[])
 {
 	int pid;
 
-	//si nos pasan menos o mas de dos argumentos
-	if (argc != 2)
-	{
+	//si nos pasan menos o mas de tres argumentos
+	if (argc < 3 || argc > 3)
 		//imprimimos que los argumentos son invalidos
-		printf("client: invalid arguments\n");
-		//termina la funcion llamada automaticamente
-		exit(0);
-	}
+		ft_putstr("client: invalid arguments\n");
 	//igualamos el pid al caracter numero uno del argumento
 	//y lo pasamos a int
-	pid = atoi(argv[1]);
-	//acaba proceso del pid y de la señal en concreto
-	kill(pid, SIGUSR1);
+	pid = ft_atoi(argv[1]);
+	message_char(pid, argv[2]);
 	return (0);
 }
